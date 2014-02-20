c15ec5befbe186a35465af6c8dec31603b3aae2c
ggysslersss <ggyssler@salestreamsoft.com>
2014-02-20 01:48:38 -0800

    add new event

--- www/js/index.js
+++ www/js/index.js
@@ -26,7 +26,20 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('deviceready', function () {
+            this.onDeviceReady;
+
+            if (navigator.notification) { // Override default HTML alert with native dialog
+                window.alert = function (message) {
+                    navigator.notification.alert(
+                        message,    // message
+                        null,       // callback
+                        "Workshop", // title
+                        'OK'        // buttonName
+                    );
+                };
+            }
+        }, false);
     },
     // deviceready Event Handler
     //
